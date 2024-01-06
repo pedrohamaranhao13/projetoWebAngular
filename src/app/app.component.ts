314b20064a277a0f07ebdfcbd3e0a1dcf2df0151
pedrohamaranhao13 <pedrinhoelloco@outlook.com.br>
2024-01-06 12:53:20 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { CriarUsuarioComponent } from './components/pages/criar-usuario/criar-usuario.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    NavbarComponent,
+    LoginComponent,
+    CriarUsuarioComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/pages/login/login.component';
+import { CriarUsuarioComponent } from './components/pages/criar-usuario/criar-usuario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "app/login",
+        component: LoginComponent
+    },
+    {
+        path: "app/criar-usuario",
+        component: CriarUsuarioComponent
+    },
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/app/login'
+    }
+];
